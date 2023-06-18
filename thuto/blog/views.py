f73f6423282292a8c4bcbf45277e17cf344551b9
AbutiWors1 <93925691+AbutiWors1@users.noreply.github.com>
2023-06-18 05:50:20 +0200

    updated ui, accept user input

--- thuto/blog/views.py
+++ thuto/blog/views.py
@@ -4,11 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is my first url')
+    return render(request, 'blog/index.html')
 
 def specific(request):
     number = 55
     return HttpResponse(number)
 
-def article(request,article_id):
-    return render(request,'blog/index.html',{'article_id':article_id})
