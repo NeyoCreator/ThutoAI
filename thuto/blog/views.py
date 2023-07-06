0b9d835f3a0d0cf1437633ea5cb613104bd07115
AbutiWors1 <93925691+AbutiWors1@users.noreply.github.com>
2023-07-06 16:26:37 +0200

    Added the model

--- thuto/blog/views.py
+++ thuto/blog/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import nltk
 
-# Create your views here.
+import nltk
+import ssl
 
-from chatterbot import Chatbot
-from ch
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+
+# nltk.download()
+
+
+# Chatbot object
+bot = ChatBot('chat',read_only=False,logic_adapters=['chatterbot.logic.BestMatch'])
+
+# TRAINING DATA BASED ON Q/A FORMAT
+list_to_train=[
+    "hi", #Question
+    "hi, there", #Answer
+
+    "what's your name?",
+    "I am Thuto",
+
+    "what is your favorite food?",
+    "I like cheese"
+]
+
+
+list_trainer = ListTrainer(bot)
+list_trainer.train(list_to_train)
 
 def index(request):
     return render(request, 'blog/index.html')
@@ -13,7 +43,7 @@ def specific(request):
     
     return HttpResponse('list')
 
-
 def getResponse(request):
     userMessage = request.GET.get('userMessage')
-    return HttpResponse(userMessage)
+    chatResponse = str(bot.get_response(userMessage))
+    return HttpResponse(chatResponse)
